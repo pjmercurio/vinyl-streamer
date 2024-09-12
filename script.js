722e2e0a12d6fdaf2a8ed15916876c232c2c42f4
Paul Mercurio <pauljosephmercurio@gmail.com>
2024-09-12 18:07:00 -0400

    add tone arm

--- script.js
+++ script.js
@@ -1,38 +1,120 @@
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
 document.addEventListener('DOMContentLoaded', function() {
     const audioElement = document.getElementById('audioElement');
     const rotatingImage = document.getElementById('vinylImage');
+    const tonearmImage = document.getElementById('tonearmImage');
 
-    // Check if the audio is playing, and rotate the image
     audioElement.addEventListener('playing', function() {
-        rotatingImage.classList.remove('paused'); // Start rotation
+        console.log(`PLAYER PLAYING! Muted: ${audioElement.muted}`);
+        fetchListeners();
     });
 
-    // If the audio is paused or fails to play, stop the rotation
-    // Also reload the audio source so pressing play again is caught up
+    // Reload the audio source so pressing play again is caught up
     audioElement.addEventListener('pause', function() {
-        rotatingImage.classList.add('paused');
+        console.log(`PLAYER PAUSED! Muted: ${audioElement.muted}`);
         const currentSrc = audioElement.children[0].src;
         audioElement.src = '';
         audioElement.src = currentSrc;
+        fetchListeners();
     });
 
     audioElement.addEventListener('error', function() {
         rotatingImage.classList.add('paused');
     });
 
-    // Initial check in case the stream is not active on page load
-    if (audioElement.paused) {
-        rotatingImage.classList.add('paused');
-    }
-
-    // If Safari is detected, modify the audio element style
-    checkIfSafari();
-
     // Fetch the listener count initially and every 20 seconds
     fetchListeners();
     setInterval(fetchListeners, 20000);
+
+    let isDragging = false;
+    let startX;
+    let currentRotation = 0;
+
+    // Function to calculate angle based on mouse movement
+    function rotateTonearm(e) {
+        if (!isDragging) return;
+
+        // Get the difference in the mouse's horizontal movement
+        const currentX = getXPosition(e);
+        const deltaX = -(currentX - startX)
+
+        // Rotate the tonearm based on the new angle
+        if (window.innerWidth < 600) {
+            currentRotation += deltaX / 4;
+            tonearmImage.style.transform = `translate(180%, -75%) rotate(${currentRotation}deg)`;
+        } else {
+            currentRotation += deltaX / 7;
+            tonearmImage.style.transform = `translate(155%, -70%) rotate(${currentRotation}deg)`;
+        }
+        
+        // Update the starting X position for the next move
+        startX = currentX;
+
+        checkOverlap();
+    }
+
+    // Function to check overlap between tonearm and vinyl
+    function checkOverlap(didLetGo = false) {
+        const transform = tonearmImage.style.transform;
+        let transformRotation = 0;
+
+        if (transform.includes('rotate')) {
+            const match = transform.match(/rotate\(([-\d.]+)deg\)/);
+            if (match) transformRotation = parseFloat(match[1]);
+        }
+
+        // Determine if the tonearm is over the vinyl and play/pause the audio
+        if (transformRotation > 5.5) {
+            vinylImage.classList.remove('paused');
+            if (didLetGo) audioElement.muted = false;
+        } else {
+            vinylImage.classList.add('paused');
+            if (didLetGo) audioElement.pause();
+        }
+    }
+
+    // Event listeners for mouse and touch events
+    function startDragging(event) {
+        console.log("START DRAGGING");
+        isDragging = true;
+        startX = getXPosition(event);
+
+        // Prevent default touch actions like scrolling
+        if (event.type === 'touchstart') {
+            event.preventDefault();
+        }
+
+        document.addEventListener(isTouchDevice ? 'touchmove' : 'mousemove', rotateTonearm);
+        document.addEventListener(isTouchDevice ? 'touchend' : 'mouseup', stopDragging);
+
+        // Start playing (muted) when the tonearm is lifted on start event to get around mobile restrictions
+        audioElement.muted = true;
+        audioElement.play();
+    }
+
+    function stopDragging(event) {
+        console.log("STOP DRAGGING!");;
+        if (isDragging) checkOverlap(true);
+        isDragging = false;
+        document.removeEventListener(isTouchDevice ? 'touchmove' : 'mousemove', rotateTonearm);
+        document.removeEventListener(isTouchDevice ? 'touchend' : 'mouseup', stopDragging);
+    }
+
+    // Prevent dragging of the tonearm image in default browser behavior
+    tonearmImage.addEventListener('dragstart', (e) => e.preventDefault());
+
+    // Mouse/Touch drag start events
+    tonearmImage.addEventListener('mousedown', startDragging);
+    tonearmImage.addEventListener('touchstart', startDragging);
 });
 
+// Utility function to get the X position from a mouse or touch event
+function getXPosition(event) {
+    return isTouchDevice ? event.touches[0].clientX : event.clientX;
+}
+
+// Function to fetch the listener count from the Icecast server
 async function fetchListeners() {
     try {
       const response = await fetch('http://paulmercurio.tplinkdns.com:8001/status-json.xsl');
@@ -45,15 +127,5 @@ async function fetchListeners() {
     } catch (error) {
       console.error('Error fetching Icecast stats:', error);
       listenerCountElement.style.display = 'none';
-      listenerCountElement.innerText = 'Error fetching listener count';
-    }
-}
-
-function checkIfSafari() {
-    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-    if (!isSafari) return;
-    const audioElement = document.getElementById('audioElement');
-    if (audioElement) {
-        audioElement.style.width = 'revert'; // Remove the fixed width for Safari
     }
 }
